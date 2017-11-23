09ee2ac820e39fe23c337f2c37c3e54a6723fe8f
Adrian Cuellar <aacuellar14@gmail.com>
2017-11-23 12:57:13 -0800

    debugging

---
+++ src/WebServer.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(8080)){
+            System.out.println("Server started");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected to server");
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),"UTF-8"));
+            String path;
+            String [] parse;
+            while((path=br.readLine())!=null) {
+                System.out.println(path);
+                parse = path.split(" ");
+                if(parse[0].equalsIgnoreCase("GET"))
+                    break;
+                else
+                    System.out.println(parse[0]);
+            }
+            parse = path.split(" ");
+            path = "www" + parse[1];
+            PrintStream out = new PrintStream(clientSocket.getOutputStream(), true, "UTF-8");
+            if(new File(path).exists()){
+                out.println(parse[2] + " 200 OK");
+                out.println("Content-type: text/html");
+                File file = new File(path);
+                BufferedReader brf = new BufferedReader(new FileReader(file));
+                out.println("Content-length: " + file.length());
+                System.out.println("");
+                String temp;
+                while((temp=brf.readLine())!=null){
+                    out.println(temp);
+                }
+            }
+            else{
+                out.println(parse[2] + " 404 Not Found");
+                out.println("Content-type: text/html");
+                File file = new File("www/FileNotFound.html");
+                BufferedReader brf = new BufferedReader(new FileReader(file));
+                out.println("Content-length: " + file.length());
+                System.out.println("");
+                String temp;
+                while((temp=brf.readLine())!=null){
+                    out.println(temp);
+                }
+                brf.close();
+            }
+            clientSocket.close();
+            br.close();
+        }
+    }
+}
